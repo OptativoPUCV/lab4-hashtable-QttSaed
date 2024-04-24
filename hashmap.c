687bf252094451c40fc85e987a085e6f5bf91c0b
QttSaed <163349961+QttSaed@users.noreply.github.com>
2024-04-24 15:33:47 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,8 +40,17 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-
-
+  if(map->size==map->capacity){
+    enlarge(map);
+  }
+  long posicion=hash(key,map->capacity);
+  while(map->buckets[posicion]!=NULL){
+    posicion++;
+    posicion=posicion%map->capacity;  
+  }
+  map->buckets[posicion]=createPair(key,value);
+  map->size++;
+  
 }
 
 void enlarge(HashMap * map) {
