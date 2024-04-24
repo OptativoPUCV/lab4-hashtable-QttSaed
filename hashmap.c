4d511b91200d5ebc0639c535d65c27908381e70e
QttSaed <163349961+QttSaed@users.noreply.github.com>
2024-04-24 16:05:34 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -103,6 +103,13 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
+  for(longi=0;i< map->capacity;i++){
+    Pair *pair = (Pair *)map->buckets[i];
+    if(pair!=NULL && pair->key !=NULL){
+      map->current=i;
+      return pair;
+    }
+  }
 
     return NULL;
 }
