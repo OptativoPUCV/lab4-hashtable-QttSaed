6636643140c13daac7db4c3772754bda7c6fdec0
QttSaed <163349961+QttSaed@users.noreply.github.com>
2024-04-24 15:44:20 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -74,8 +74,15 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
+  HashMap * mapa=(HashMap*)malloc(sizeof(HashMap));
+  if(map==NULL)return NULL;
+  mapa->buckets=(Pair**)calloc(capacity,sizeof(Pair*)); 
+  if(map->buckets==NULL)return NULL;
+  mapa->size=0; 
+  mapa->capacity=capacity;
+  mapa->current=-1;
+  return mapa;
   
-    return NULL;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
